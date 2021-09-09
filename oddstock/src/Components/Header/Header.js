e84ebf12a05b4e11f150815d84130b86ffd1376e
Bhargav-2808 <bhargavvalani01@gmail.com>
2021-09-09 16:32:56 +0530

    update the files

--- oddstock/src/Components/Header/Header.js
+++ oddstock/src/Components/Header/Header.js
@@ -1,8 +1,8 @@
 import './Header.css';
 import React from 'react';
 import { Navbar,Nav,Container} from 'react-bootstrap';
-import LockIcon from '@material-ui/icons/Lock';
-import LockOpenIcon from '@material-ui/icons/LockOpen';
+//import LockIcon from '@material-ui/icons/Lock';
+//import LockOpenIcon from '@material-ui/icons/LockOpen';
 
 export default function Header(porps) {
     return (
@@ -18,8 +18,8 @@ export default function Header(porps) {
                         <Nav.Link href="#premium" style={{color:"#782df3",fontWeight: "600"}} className="px-lg-4">Premium</Nav.Link>
                         {
                             porps.login?
-                            <Nav.Link href="#pricing" style={{backgroundColor:"#E1EFFF",borderRadius: "0.5rem",fontWeight: "500"}} className="px-lg-4"><LockOpenIcon/> {porps.accountHolder}</Nav.Link>:
-                            <Nav.Link href="#account" className="px-lg-4"><LockIcon/> Account</Nav.Link>
+                            <Nav.Link href="#pricing" style={{backgroundColor:"#E1EFFF",borderRadius: "0.5rem",fontWeight: "500"}} className="px-lg-4">{porps.accountHolder}</Nav.Link>:
+                            <Nav.Link href="#account" className="px-lg-4"> Account</Nav.Link>
                         }
                     </Nav>
                 </Navbar.Collapse>
