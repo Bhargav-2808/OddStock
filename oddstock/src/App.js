bbdd285c9a1d32a07c04926dbd32dcb6fe75127f
Bhargav-2808 <bhargavvalani01@gmail.com>
2021-09-11 12:22:29 +0530

    searrch Intialized

--- oddstock/src/App.js
+++ oddstock/src/App.js
@@ -4,12 +4,14 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 import About from '../src/Components/About/About';
 import Header from '../src/Components/Header/Header';
+import Search from "../src/Components/Search/Search";
 
 function App() {
   return (
     <>
     <Header accountHolder="Dhruv" login={true}/>
     <About/>
+    <Search/>
     </>
   );
 }
--- oddstock/src/Components/Search/Search.js
+++ oddstock/src/Components/Search/Search.js
@@ -1,1 +1,28 @@
 import './Search.css';
+import react from 'react'
+
+import { Container, Row, Col } from 'react-bootstrap';
+
+const  Search =()=>{
+    return(
+        <>
+            <Container className="mt-5">
+                <Row >
+                    <Col >
+                        <h1 className="centerComponent searchTitle">Search the Stock Here</h1>
+                        <hr/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div className="search">
+                            <input type="Search"/>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
+}
+
+export default Search;
