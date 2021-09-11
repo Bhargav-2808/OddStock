8286a04f67c167ed692c67163318dd9f24adcbf8
Bhargav-2808 <bhargavvalani01@gmail.com>
2021-09-11 15:45:14 +0530

    updated search

--- oddstock/src/Components/Search/Search.js
+++ oddstock/src/Components/Search/Search.js
@@ -1,23 +1,26 @@
 import './Search.css';
+import SearchIcon from '@material-ui/icons/Search';
 import react from 'react'
 
 import { Container, Row, Col } from 'react-bootstrap';
 
-const  Search =()=>{
-    return(
+const Search = () => {
+    return (
         <>
             <Container className="mt-5">
                 <Row >
                     <Col >
                         <h1 className="centerComponent searchTitle">Search the Stock Here</h1>
-                        <hr/>
+                        <hr />
                     </Col>
                 </Row>
                 <Row>
                     <Col>
-                        <div className="search">
-                            <input type="Search"/>
-                        </div>
+                        <form action="">
+                            <input type="search"/>
+                            <SearchIcon/>
+                            <a id="clear-btn">Clear</a>
+                        </form>
                     </Col>
                 </Row>
             </Container>
