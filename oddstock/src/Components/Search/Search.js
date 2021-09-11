9be57381f9b8ecefb7788f79bf07ed07fdf07ec9
Bhargav-2808 <bhargavvalani01@gmail.com>
2021-09-11 18:36:36 +0530

    search updates

--- oddstock/src/Components/Search/Search.js
+++ oddstock/src/Components/Search/Search.js
@@ -11,15 +11,15 @@ const Search = () => {
             <Container className="mt-5">
                 <Row >
                     <Col >
-                        <h1 className="centerComponent searchTitle">Search the Stock Here</h1>
+                        <h1 className="centerComponent searchTitle"></h1>
                         <hr />
                     </Col>
                 </Row>
                 <Row>
                     <Col>
                         <div className="search-form centerComponent mt-5">
-                            <input type="search"  className="search-input"/>
-                            <SearchRoundedIcon className="search-icon"/>
+                            <input type="search"  className="search-input" placeholder="Type a company name" autoComplete="off" autoFocus />
+                            
                         </div>
                     </Col>
                 </Row>
