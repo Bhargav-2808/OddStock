83a22f74e7a29e9b0e1af720379586126c02bbc0
Bhargav-2808 <bhargavvalani01@gmail.com>
2021-10-29 20:41:50 +0530

    newz updated

--- oddstock/src/Components/News/News.js
+++ oddstock/src/Components/News/News.js
@@ -27,14 +27,14 @@ const News = () => {
                     {
                         newsHeadline.map((element)=>{
                             return <div key={element.url} className="py-4" style={{width:'100%'}}>
-                                        <Row>
+                                        <Row className="newsImage" >
                                             <Col>
                                                 <Card.Title>{element.title}</Card.Title>
                                                 <a href={element.url} target="_blank">
                                                     <Button variant="primary" style={{width:"7rem"}}>Read More</Button>
                                                 </a>
                                             </Col>
-                                            <Col>
+                                            <Col className="newsImage">
                                                 <Image src={element.urlToImage
                                               ?element.urlToImage:"https://images.unsplash.com/photo-1585776245991-cf89dd7fc73a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8bmV3c3x8fHx8fDE2MzAyMzE1MzI&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1600"} rounded />
                                             </Col>
